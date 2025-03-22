e2ec4f0bf43b9d388cb513f42d5376396a567029
ben <bwall4@gmu.edu>
2025-03-21 20:29:34 -0400

    add env map?

--- shader/skel-frag.glsl
+++ shader/skel-frag.glsl
@@ -49,6 +49,47 @@ brdf_attenuated(vec3 diffuse_color, vec3 f0, float roughness, vec3 n, vec3 l, ve
     return (Fd + Fr) * NoL;
 }
 
+vec3
+brdf_envmap(vec3 diffuse_color, vec3 f0, float roughness, vec3 n, vec3 l, vec3 v) {
+    
+
+    // Use this if we get an env map.
+    //float max_level = 1.0 + floor(log2(textureSize(environment_map, 0).x));
+    //vec3 sampled_Id = textureLod(environment_map, n, max_level - 1).rgb;
+    // Use roughness^1/8 instead of perceptual_roughness^1/4
+    // Saves one parameter.
+    //vec3 sampled_Il = textureLod(environment_map, l, (max_level - 1) * pow(roughness, 0.125)).rgb;
+
+    // Otherwise, just do constant color.
+    vec3 sampled_Id = vec3(0.3, 0.5, 0.9) * 0.5;
+    vec3 sampled_Il = sampled_Id;
+
+    // Initialize Fd to 0 in case we do the sum
+    vec3 Fd = vec3(0.0);
+    // Direct lookup
+    Fd = diffuse_color * sampled_Id;
+
+    // For direct image-based specular, use the reflected view vector as the light
+    // vector.
+    l = reflect(-v, n);
+
+    vec3 h = normalize(v + l);
+    float NoV = abs(dot(n, v)) + 1e-5;
+    float NoL = clamp(dot(n, l), 0.0, 1.0);
+    float NoH = clamp(dot(n, h), 0.0, 1.0);
+    float LoH = clamp(dot(l, h), 0.0, 1.0);
+
+    float D = D_GGX(NoH, roughness);
+    vec3 F = F_Schlick(LoH, f0);
+    float V = V_SmithGGXCorrelated(NoV, NoL, roughness);
+
+    vec3 Fr = (D * V) * F;
+
+    Fr = PI * Fr * sampled_Il * NoL / D;
+
+    return Fd + Fr;
+}
+
 void main() {
     vec3 normal = normalize(v_norm);
 
@@ -69,6 +110,15 @@ void main() {
     vec3 v = -normalize(v_pos);
 
     sum = brdf_attenuated(
+        diffuse_color,
+        f0,
+        roughness,
+        normal,
+        -normalize(light_direction),
+        v_pos
+    );
+
+    sum += brdf_envmap(
         diffuse_color,
         f0,
         roughness,
